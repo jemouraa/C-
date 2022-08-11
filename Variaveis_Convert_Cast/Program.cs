854ef9f16c3e094f78de75c88fe10be22f443465
Jeruza de Moura <jecruz12@gmail.br>
2022-08-11 09:12:04 -0300

    variaveis

---
+++ Variaveis_Convert_Cast/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var nome = "Jeruza";
+
+var idade = 31;
+
+Animal a = new Animal();
+
+Jacare j = (Jacare)a;
+
+Console.WriteLine(nome + idade);
+
+
+public class Animal
+{
+
+}
+
+public class Jacare : Animal
+{
+
+}
+
+
+
